81abb60dc48bf21c1821497118da68669dda7e99
jisbruzzi <jose.sbru@gmail.com>
2018-09-17 17:57:26 -0300

    Un script arma todas las imágenes y lanza los test

--- app/index.js
+++ app/index.js
@@ -6,26 +6,25 @@ const pool = Pool({
     host:"db",
     port:5432,
     user:"postgres",
-    database:"postgres"
-    //connectionString:"postgresql://pg-user:pg-user@localhost:5432/pg-prueba"
+    database:"postgres",
+    connectionTimeoutMillis:20000 //sin esto, puede ser que el server levante antes que postgres y rompa todo
 })
 // cómo estructurar bien el proyecto https://node-postgres.com/guides/async-express
 var app = express();
 
 /*
-ESTE ENDPOINT ES NECESARIO PARA QUE EL FRAMEWORK DE TEST SEPA QUE EL SERVER EXISTE Y ESTÁ DESPIERTO
+ESTE ENDPOINT ES NECESARIO PARA QUE EL FRAMEWORK DE TEST SEPA QUE EL SERVER EXISTE Y ESTÁ DESPIERTO, Y QUE SE HABLA CON LA BASE DE DATOS. Espera esta respuesta antes de arrancar con los test.
 */
-
 app.get('/', function (req, res) {
     pool.connect().then((c)=>{
-        c.query("select texto from tabla_prueba2;").then((resultado)=>{
+        return c.query("select texto from tabla_prueba2;").then((resultado)=>{
             res.send(resultado.rows)
         })
-    })  
+    }).catch((e)=>{
+        console.log(e)
+    })
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-//van 30m
--- test/wait-and-run.sh
+++ test/wait-and-run.sh
@@ -1,2 +1,3 @@
-curl http://app:3000/
+sleep 5
+curl --connect-timeout 30 http://app:3000/
 npm test
