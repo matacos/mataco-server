0bedb17164c1c3573cac814f246d551b86a3cb2e
jisbruzzi <jose.sbru@gmail.com>
2018-09-17 01:01:36 -0300

    AGrego base de datos de prueb con un .sql que la construye, funciona cuando se corre la app desde afuera

---
+++ db/setup.sql
@@ -0,0 +1,4 @@
+create table tabla_prueba (
+ texto text 
+);
+insert into tabla_prueba (texto) values ('Hola desde setup.sql');
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require('express');
+const Pool = require("pg").Pool;
+
+const pool = Pool({
+    host:"127.0.0.1",
+    port:5444,
+    user:"postgres",
+    database:"postgres"
+    //connectionString:"postgresql://pg-user:pg-user@localhost:5432/pg-prueba"
+})
+// cómo estructurar bien el proyecto https://node-postgres.com/guides/async-express
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    pool.connect().then((c)=>{
+        c.query("select texto from tabla_prueba;").then((resultado)=>{
+            res.send(resultado.rows[0])
+        })
+    })  
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+//van 30m
