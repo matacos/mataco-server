655f0f11d4c59a12c8ae7a839b920a6481fec1bd
jisbruzzi <jose.sbru@gmail.com>
2018-09-21 16:16:11 -0300

    #2 Agrego todo lo referente a materias y alumnos a la base de datos

--- db/setup.sql
+++ db/setup.sql
@@ -3,6 +3,10 @@ create table tabla_prueba2 (
 );
 insert into tabla_prueba2 (texto) values ('Hola desde setup.sql');
 
+
+/**********************
+LOGIN
+***********************/
 create table users (
     username varchar(10),
     password varchar(30),
@@ -14,9 +18,18 @@ create table users (
     unique (token)
 );
 
+insert into users (username,password,email,token,token_expiration) values
+    ('jose','jojo','jose.jose@gmail.com','78',now()+'5 minutes'),
+    ('97452','jojo','jose.jose@gmail.com','79',now()+'5 minutes'),
+    ('99999','9','nina.niner@gmail.com','99',now()+'5 minutes'),
+    ('gryn','777','sebas@fi.uba.ar','18',now()+'5 minutes');
+
+
+/******************************************
+ROLES
+*******************************************/
 create table students(
     username varchar(10),
-    degree varchar(10),
     primary key (username),
     foreign key (username) references users(username)
 );
@@ -39,14 +52,83 @@ create table professors(
     foreign key (username) references users(username)
 );
 
-insert into users (username,password,email,token,token_expiration) values
-    ('jose','jojo','jose.jose@gmail.com','78',now()+'5 minutes');
-insert into users (username,password,email,token,token_expiration) values
-    ('97452','jojo','jose.jose@gmail.com','79',now()+'5 minutes');
-insert into users (username,password,email,token,token_expiration) values
-    ('gryn','777','sebas@fi.uba.ar','18',now()+'5 minutes');
-
-insert into students (username,degree) values ('jose','1');
-insert into students (username,degree) values ('97452','1');
+insert into students (username) values ('jose'),('97452');
 insert into professors (username) values ('gryn');
 insert into department_administrators (username) values ('gryn');
+
+
+/*********************************************
+MATERIAS Y CURSOS
+**********************************************/
+create table degrees(
+    id varchar(10),
+    name text,
+    primary key (id)
+);
+insert into degrees (id,name) values
+    ('1','Ingeniería CIVIL'),
+    ('2','Ingeniería INDUSTRIAL'),
+    ('3','Ingeniería NAVAL Y MECÁNICA'),
+    ('4','AGRIMENSURA'),
+    ('5','Ingeniería MECÁNICA'),
+    ('6','Ingeniería ELECTRICISTA'),
+    ('7','Ingeniería ELECTRÓNICA'),
+    ('8','Ingeniería QUÍMICA'),
+    ('9','Licenciatura en ANÁLISIS DE SISTEMAS'),
+    ('10','Ingeniería en INFORMÁTICA'),
+    ('11','Ingeniería de ALIMENTOS');
+
+create table degree_enrollments (
+    degree varchar(10),
+    student varchar(10),
+    foreign key (degree) references degrees(id),
+    foreign key (student) references students(username),
+);
+insert into degree_enrollments( degree, student) values 
+    ('1','97452'),
+    ('1','99999'),
+    ('2','99999'),
+    ('3','99999');
+
+
+create table departments(
+    code varchar(10),
+    name text,
+    primary key (code),
+);
+
+create table subjects(
+    name text,
+    code varchar(10),
+    department varchar(10),
+    primary key (code, department),
+    foreign key (department) references departments(code)
+);
+
+create table credits(
+    subject_code varchar(10),
+    department_code varchar(10),
+
+    degree varchar(10)
+    amount int,
+    primary key (degree,subject_code,department_code),
+    foreign key (subject_code,department_code) references subjects(code,department)
+);
+
+create table requires_credits(
+    department_code varchar(10),
+    subject_code varchar(10),
+    amount int,
+    primary key (department_code,subject_code),
+    foreign key (department_code,subject_code) references subjects(code,department)
+)
+
+create table requires (
+    department_code varchar(10),
+    subject_code varchar(10),
+    dept_required varchar(10),
+    code_required varchar(10),
+    primary key (department_code, subject_code, dept_required, code_required),
+    foreign key (department_code, subject_code) references subjects(code,department),
+    foreign key (department_code, subject_code) references subjects(code_required,dept_required),
+)
--- run-setup-db.sh
+++ run-setup-db.sh
@@ -1,4 +1,5 @@
 cd db
 heroku login
 sudo su - postgres -c "psql $(heroku config:get DATABASE_URL -a mataco)" < destroy.sql
+sudo su - postgres -c "psql $(heroku config:get DATABASE_URL -a mataco)" < materias_deptos.sql
 sudo su - postgres -c "psql $(heroku config:get DATABASE_URL -a mataco)" < setup.sql
