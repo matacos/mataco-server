fcce0d15ed50fb045a5b565b3dce225404156d47
Camilaf <cdetrincheria@hotmail.com>
2018-09-23 03:47:56 -0300

    Create Panel to refactor

---
+++ front/src/components/Panel.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logoFIUBA from '../images/logo.png'
+
+class Panel extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+        <div className="panel panel-default col-md-3" style={{margin: "0", padding: "0"}}>
+            <div className="panel-heading" style={{width: "auto"}}>
+                <img className="img-responsive center-block" src={logoFIUBA} height="50%" width="50%" style={{marginLeft: "auto", marginRight: "auto", width: "50%", paddingTop: "1em"}}/>
+                <h3 className="panel-title text-center" style={{padding: "1em"}}>Carlos Fontela</h3>
+            </div>
+
+            <div className="panel-body" style={{height: "100vh"}}>
+                <div style={{padding: "1em"}}>
+                <button className="text-primary" style={{background: "none", border: "none", paddingBottom: "1em"}} onClick={this.props.handleSelectedMenu}> {this.props.text} </button>
+                {this.props.selectedCourses && 
+                <div>{this.props.listItems}</div>}
+                </div>
+            </div>
+        </div>);
+    }
+}
+
+export default Panel;
