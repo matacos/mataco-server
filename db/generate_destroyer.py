36fd0127cfa7a96d924a4a5634232c6d65adf3b7
jisbruzzi <jose.sbru@gmail.com>
2018-09-24 19:48:19 -0300

    mejoro el sql

--- db/generate_destroyer.py
+++ db/generate_destroyer.py
@@ -1,7 +1,7 @@
 import re
 with open("destroy.sql","w") as destroy_sql:
-    for fname_setup in ["setup.sql","setup_postmaterias.sql"]:
-        with open("setup.sql") as setup_sql:
+    with open("setup.sql") as setup_sql:
+        for fname_setup in ["setup.sql","setup_postmaterias.sql"]:
             for line in setup_sql:
                 result = re.search("create table ([a-zA-Z_]+)",line)
                 try:
