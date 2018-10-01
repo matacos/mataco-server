963ffd740932892a71f376fa04786f46adfc80fc
Camilaf <cdetrincheria@hotmail.com>
2018-10-01 00:24:19 -0300

    Proxy + local storage

--- front/src/Proxy.js
+++ front/src/Proxy.js
@@ -1,3 +1,5 @@
+import Assistant from "./Assistant";
+
 class Proxy  {
     constructor() {
         this.url = "http://mataco.herokuapp.com/api"
@@ -18,6 +20,48 @@ class Proxy  {
              }).then(res => res.json())
 
       }
+
+      getDepartmentSubjects(department_name) {
+        return fetch(this.url + "/materias?departamento=" + department_name, {
+            method: 'GET',
+            headers: {
+              'Authorization': 'bearer ' + Assistant.getField("token")
+            },
+         }).then(res => res.json()) 
+      }
+
+      getProfessorCourses(professorDni) {
+        return fetch(this.url + "/cursos?profesor=" + professorDni, {
+                method: 'GET',
+                headers: {
+                  'Authorization': 'bearer ' + Assistant.getField("token")
+                },
+             }).then(res => res.json()) 
+
+      }
+
+      getSubjectCourses(department_code, subject_code) {
+        return fetch(this.url + "/cursos?cod_departamento=" + department_code + "&cod_materia=" + subject_code, {
+                method: 'GET',
+                headers: {
+                  'Authorization': 'bearer ' + Assistant.getField("token")
+                },
+             }).then(res => res.json()) 
+
+      }
+
+      getCourseStudents(course) {
+        return fetch(this.url + "/cursadas?curso=" + course , {
+                method: 'GET',
+                headers: {
+                  'Authorization': 'bearer ' + Assistant.getField("token")
+                },
+             }).then(res => res.json()) 
+
+      }
+
+
+
   }
   
   export default new Proxy();
