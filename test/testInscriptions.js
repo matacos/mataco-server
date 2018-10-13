b56f1fc317236139ea9e6d1eb15424b706d84594
jisbruzzi <jose.sbru@gmail.com>
2018-10-13 17:35:37 -0300

    #54 agrego los tests de inscripciones, restauran el estado inicial (casi)

--- test/testInscriptions.js
+++ test/testInscriptions.js
@@ -107,25 +107,34 @@ describe("Test /inscripciones_cursos",()=>{
             expect(inscription.course.course).to.be.equal(1)
         }
     })
-    /*
+    
     it("add 97452 to course 2 (he is enrolled in course 1 already), and then remove him from course 1",async ()=>{
-        let response = await requestWithAuth("97452","jojo","POST","/cursadas/",{
+        
+        //chequeo que haya 1 inscripción
+        let response = await requestWithAuth("97452","jojo","GET","/inscripciones_cursos?estudiante=97452")
+        expect(response.body.courseInscriptions).to.have.lengthOf(1)
+        let cursoInicial = response.body.courseInscriptions[0]
+
+        //inscribo al estudiante al curso 1
+        response = await requestWithAuth("97452","jojo","POST","/cursadas/",{
             "student":"97452",
             "course":"2"
         })
         expect(response.statusCode).to.equal(201)
+
+        //chequeo que haya 2
         response = await requestWithAuth("97452","jojo","GET","/inscripciones_cursos?estudiante=97452")
         expect(response.body.courseInscriptions).to.have.lengthOf(2)
 
-
+        //desinscribo al estudiante del curso 2
         response = await requestWithAuth("97452","jojo","DELETE","/cursadas/2-97452")
-        console.log(response.body)
         expect(response.statusCode).to.equal(204)
+
+        // chequeo que haya 1
         response = await requestWithAuth("97452","jojo","GET","/inscripciones_cursos?estudiante=97452")
-        console.log(response.body)
         expect(response.body.courseInscriptions).to.have.lengthOf(1)
 
-
+        // le pongo nota
         let date=(new Date()).toISOString()
         response = await requestWithAuth("97452","jojo","PUT","/cursadas/1-97452",{
             accepted:"true",
@@ -134,23 +143,38 @@ describe("Test /inscripciones_cursos",()=>{
         })
         expect(response.statusCode).to.equal(204)
 
+        // chequeo que haya cambiado la nota
         response = await requestWithAuth("97452","jojo","GET","/cursadas?curso=1")
-        //ese request es un alumno poniendose la nota :D
-        console.log("=======")
-        console.log("=======")
-        console.log("=======")
-        console.log("=======")
-        console.log(response.body)
-        console.log("=======")
-        console.log("=======")
-        console.log("=======")
-        
         expect(response.body.courseInscriptions[0].accepted).to.be.true
         expect(response.body.courseInscriptions[0].grade).to.be.equal("6")
 
-        await requestWithAuth("97452","jojo","DELETE","/cursadas?curso=1")
+        //desinscribo al estudiante del curso 1
+        response = await requestWithAuth("97452","jojo","DELETE","/cursadas/1-97452")
+        expect(response.statusCode).to.equal(204)
+
+        //inscribo al estudianye al curso 2, con el curso de antes
+        let newCourse={
+            student:cursoInicial.student.username+"",
+            course:cursoInicial.course.course+""
+        }
+        response = await requestWithAuth("97452","jojo","POST","/cursadas/",newCourse)
+        /*
+        console.log("======")
+        console.log(cursoInicial)
+        console.log("======")
+        console.log(response.body.validations.body)
+        console.log("======")
+        console.log(response.body)
+        console.log("======")
+        console.log(response.body.jsonSchemasValidated)
+        console.log("======")
+        */
+        expect(response.statusCode).to.equal(201)
+
+        // chequeo que haya 1
+        response = await requestWithAuth("97452","jojo","GET","/inscripciones_cursos?estudiante=97452")
+        expect(response.body.courseInscriptions).to.have.lengthOf(1)
         
     })
-    */
 
 })
