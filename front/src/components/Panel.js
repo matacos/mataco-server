76d6d0fa5479e3a0b869f944c6aea671ac30e440
Camilaf <cdetrincheria@hotmail.com>
2018-10-18 01:06:03 -0300

    #65 Modal para crear un final + validaciones

--- front/src/components/Panel.js
+++ front/src/components/Panel.js
@@ -3,8 +3,12 @@ import logoFIUBA from '../images/logo.png';
 import Assistant from '../Assistant';
 import Proxy from '../Proxy';
 import { withRouter } from 'react-router-dom';
-import { Glyphicon, DropdownButton, MenuItem, ButtonGroup } from 'react-bootstrap';
+import { Glyphicon, DropdownButton, MenuItem, ButtonGroup, Modal, Button } from 'react-bootstrap';
 import "./Panel.css";
+import 'moment/locale/es.js';
+import { DatePickerInput } from 'rc-datepicker';
+
+import 'rc-datepicker/lib/style.css';
 
 class Panel extends Component {
     constructor(props) {
@@ -14,9 +18,25 @@ class Panel extends Component {
             subjects: [],
             courses: [],
             exams: [],
-            currentCourse: null
+            currentCourse: null,
+            showAddExam: false,
+            examData: {
+                classroom: '',
+                place: '',
+                date: this.getDate(),
+                beginning: '',
+                ending: ''
+            },
+            inputError: false,
+            errorMsg: ''
         };
 
+    }
+
+    getDate() {
+        var date = new Date();
+        console.log(date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate())
+        return date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
     }
 
     setExams() {
@@ -95,21 +115,68 @@ class Panel extends Component {
         this.props.history.push('/finales/' + this.state.currentCourse.department_code + this.state.currentCourse.subject_code + '/' + examId);
     }
 
-    addExam() {
-        let exam = {
-            semester_code: "1c2018",
-            department_code: this.state.currentCourse.department_code,
-            subject_code: this.state.currentCourse.subject_code,
-            examiner_username: Assistant.getField("username"),
-            classroom_code: "200",
-            classroom_campus: "Paseo Colón",
-            beginning: "16:55",
-            ending: "19:00",
-            exam_date: "2018-04-04"
-        }
+    validInput() {
+        var errorMsg = "Debe completar todos los campos para agregar un examen";
+        var begin = this.state.examData.beginning.split(":");
+        var end = this.state.examData.ending.split(":");
 
-        Proxy.addExam(exam)
-        .then(this.setExams());
+        if (this.state.examData.classroom.length == 0)
+            return [false, errorMsg];
+
+        let dateValidation = this.validDate();
+        if (!dateValidation[0])
+            return dateValidation;
+
+        return this.validTime(begin[0], begin[1], end[0], end[1]);
+    }
+
+    validTime(beginningHour, beginningMinutes, endingHour, endingMinutes) {
+        if (beginningHour.length > 0 && beginningMinutes.length > 0 && endingHour.length > 0 && endingMinutes.length > 0)
+            if ((beginningHour < endingHour) || ((beginningHour == endingHour) && (beginningMinutes < endingMinutes)))
+                return [true, ""];
+            else
+                return [false, "Recuerde que el horario de finalización debe ser posterior al horario de inicio"];
+        return [false, "Debe completar todos los campos para agregar un examen"];
+    }
+
+    validDate() {
+        let date = new Date(this.state.examData.date);
+        let currentDate = new Date();
+        if (date.getFullYear() < currentDate.getFullYear())
+            return [false, "No es posible crear un examen para un año anterior al actual"];
+        if ((date.getFullYear() == currentDate.getFullYear()) && (date.getMonth() < currentDate.getMonth()))
+            return [false, "No es posible crear un examen para un mes que ya pasó"];
+        if ((date.getMonth() == currentDate.getMonth()) && (date.getDate() < currentDate.getDate()))
+            return [false, "No es posible crear un examen para un día que ya pasó"];
+        return [true, ""];
+    }
+
+    addExam() {
+        if (this.state.showAddExam) {
+            var newExam = null;
+            var date = new Date(this.state.examData.date);
+            let validationResult = this.validInput();
+            if (validationResult[0]) {
+                newExam = {
+                    semester_code: "1c2018",
+                    department_code: this.state.currentCourse.department_code,
+                    subject_code: this.state.currentCourse.subject_code,
+                    examiner_username: Assistant.getField("username"),
+                    classroom_code: this.state.examData.classroom,
+                    classroom_campus: this.state.examData.place.length == 0 ? "Paseo Colón" : this.state.examData.place,
+                    beginning: this.state.examData.beginning,
+                    ending: this.state.examData.ending,
+                    exam_date: date.getFullYear() + "-" + (date.getMonth() + 1)+ "-" + date.getDate() 
+                }
+                
+                Proxy.addExam(newExam)
+                .then(this.setExams());
+                this.handleHide();
+            }
+            else {
+                this.setState({inputError: true, errorMsg: validationResult[1]})
+            }
+        }
     }
 
     showCourseDropdown(courses) {
@@ -156,7 +223,7 @@ class Panel extends Component {
                                 <hr />
                                 {examsList}
                                 {examsList.length < 5 && <div key="Add-exam">
-                                <button className="text-primary text-left Panel-list-item" onClick={this.addExam.bind(this)}>
+                                <button className="text-primary text-left Panel-list-item" onClick={this.showAddExamModal.bind(this)}>
                                     Agregar final
                                 </button></div>}
                             </div>}
@@ -209,6 +276,22 @@ class Panel extends Component {
         );
     }
 
+    showAddExamModal() {
+        this.setState({showAddExam: true})
+    }
+
+    handleHide() {
+        let clearData = {
+            classroom: '',
+            place: '',
+            date: this.getDate(),
+            beginning: '',
+            ending: ''
+        };
+
+        this.setState({showAddExam: false, examData: clearData});
+    }
+
     render() {
         return (
             <div>
@@ -221,6 +304,101 @@ class Panel extends Component {
             <div className="panel-body" style={this.props.selected["subjects"] ? {height: "auto"} : {height: "100vh"}}>
                 {this.showMenu(this.props.mode)}
             </div>
+
+            {this.state.showAddExam &&  <Modal
+            show={this.state.showAddExam}
+            onHide={this.handleHide.bind(this)}
+            container={this}
+            aria-labelledby="contained-modal-title"
+            >
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title">
+                Agregar final
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <form className="form-horizontal">
+            <fieldset>
+                <div className="form-group">
+                <label htmlFor="select" className="col-lg-2 control-label">Sede</label>
+                <div className="col-lg-10">
+                    <select value={this.state.examData.place} className="form-control" id="sede" onChange={ e => {
+                        var newData = this.state.examData;
+                        newData.place = e.target.value;
+                        this.setState({ examData : newData });
+                        }}>
+                    <option>Paseo Colón</option>
+                    <option>Las Heras</option>
+                    </select>
+                </div>
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="inputAula" className="col-lg-2 control-label">Aula</label>
+                <div className="col-lg-3">
+                    <input type="text" value={this.state.examData.classroom} className="form-control" id="inputAula" onChange={ e => {
+                        const re = /^[a-zA-Z0-9]+$/;
+
+                        if ((e.target.value == "" || re.test(e.target.value)) && (e.target.value.length < 100)) {
+                            var newData = this.state.examData;
+                            newData.classroom = e.target.value;
+                            this.setState({examData: newData});
+                        }
+                    } }/>
+                </div>
+                </div>
+            
+                
+                <div className="form-group">
+                <label htmlFor="select" className="col-lg-2 control-label">Fecha</label>
+                <div className="col-lg-3">
+                    <DatePickerInput
+                    onChange={value => {
+                        var newData = this.state.examData;
+                        newData.date = value;
+                        this.setState({ examData : newData });
+                        }}
+                    value={this.state.examData.date}
+                    className='my-custom-datepicker-component'
+                    />
+                </div>
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="inputInicio" className="col-lg-2 control-label">Horario de inicio</label>
+                <div className="col-lg-3">
+                    <input type="time" value={this.state.examData.beginning} className="form-control" id="inputInicio" onChange={ e => {
+                        var newData = this.state.examData;
+                        newData.beginning = e.target.value;
+                        this.setState({ examData : newData });
+                        }}/>
+                </div>
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="inputFin" className="col-lg-2 control-label">Horario de fin</label>
+                <div className="col-lg-3">
+                    <input type="time" value={this.state.examData.ending} className="form-control" id="inputFin" onChange={ e => {
+                        var newData = this.state.examData;
+                        newData.ending = e.target.value;
+                        this.setState({ examData : newData });
+                        }}/>
+                </div>
+                </div>
+
+            </fieldset>
+            </form>
+            {this.state.inputError && 
+                        <div className="alert alert-dismissible alert-danger" >
+                            <button type="button" className="close" data-dismiss="alert" onClick={ e => this.setState({ inputError : false }) }>&times;</button>
+                            <a href="#" className="alert-link"/>{this.state.errorMsg}
+                        </div>}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.handleHide.bind(this)}>Cancelar</Button>
+                <Button bsStyle="primary" onClick={this.addExam.bind(this)}>Aceptar</Button>
+            </Modal.Footer>
+            </Modal>}
             </div>}
             </div>
         );
