ecd4a0ca0eb80f170a6a761087e623e28a332699
jisbruzzi <jose.sbru@gmail.com>
2018-10-20 09:51:34 -0300

    #50 El endpoint que sube csv a postgres

--- app/routes/publicRoutes/index.js
+++ app/routes/publicRoutes/index.js
@@ -2,6 +2,7 @@ const Router = require('express-promise-router')
 const multer=require("multer")
 var storage = multer.memoryStorage()
 var upload = multer({ storage: storage })
+const Readable = require('stream').Readable;
 
 function mountRoutes(app,db,schemaValidation){
     /*
@@ -15,7 +16,41 @@ function mountRoutes(app,db,schemaValidation){
     });
 
     promiseRouter.post("/estudiantes/csv",upload.single("csv"),async function(req,res,next){
+        /*
+        Padrón
+        Contraseña inicial (en el caso de que este usuario ya exista en el sistema, este campo puede dejarse en blanco)
+        Nombre
+        Apellido
+        Prioridad
+        Email
+        Lista de carreras a las que está anotado, como enteros separados por guiones.
+        */
         const fileText=req.file.buffer.toString("utf8")
+        await db.query("drop table if exists students_upload;")
+        await db.query(`
+        create table students_upload (
+            username varchar(10),
+            password varchar(30),
+            name text,
+            surname text,
+            priority decimal,
+            email varchar(30),
+            degrees text
+        );
+        `)
+        const csvStream = new Readable();
+        csvStream.push(fileText)
+        csvStream.push(null)
+        let postgresStream = await db.copyFrom("COPY students_upload FROM STDIN with (format 'csv');")
+        csvStream.on("end",()=>{
+            console.log("LA SUBIDA A POSTGRES TERMINA A LAS: ",new Date())
+        })
+        console.log("LA SUBIDA A POSTGRES EMPIEZA A LAS: ",new Date())
+        csvStream.pipe(postgresStream)
+
+
+        
+        
         console.log("_______________")
         console.log("_______________")
         console.log("_______________")
