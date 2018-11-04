045d8c4db1fcb068c9b11183b8a749e0dfc27f4f
jisbruzzi <jose.sbru@gmail.com>
2018-11-03 21:40:19 -0300

    #70 agrego ese middleware

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,16 @@ const publicRoutes=require("./publicRoutes")
 const privateRoutes=require("./privateRoutes")
 
 function mountRoutes(app,db){
+
+    app.use(function (req,res,next){
+        if(req.query.now){
+            req.now=Date.parse(req.query.now)
+        }else{
+            req.now=new Date()
+        }
+        next()
+    })
+
     //monto /login y /logout
     security.mountRoutes(app,db,validation.schemaValidationMiddlewares)
 
