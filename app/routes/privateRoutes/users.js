3650adf36c1447deea19254708afd08e8369e46c
jisbruzzi <jose.sbru@gmail.com>
2018-11-18 09:17:01 -0300

    #148 agrego el PUT de /me

--- app/routes/privateRoutes/users.js
+++ app/routes/privateRoutes/users.js
@@ -12,6 +12,54 @@ function mountRoutes(app,db,schemaValidation){
             surname:u.surname,
             roles:u.roles,
         }})
+        next()
+    })
+    const putMeBody={required:[
+        "password",
+        "email",
+        "name",
+        "surname"
+    ]}
+    app.put("/me",schemaValidation({body:putMeBody}), async(req,res,next)=>{
+        const {
+            password,
+            email,
+            name,
+            surname
+        } = req.body;
+        const query=`
+        update users
+        set
+            password=$1,
+            email=$2,
+            name=$3,
+            surname=$4
+        where
+            username=$5
+        ;`
+        await db.query(query,[
+            password,
+            email,
+            name,
+            surname,
+            req.user.username
+        ])
+        let u=req.user
+        u.password=password
+        u.email=email
+        u.name=name
+        u.surname=surname
+        
+        res.json({"me":{
+            username:u.username,
+            password:u.password,
+            email:u.email,
+            name:u.name,
+            surname:u.surname,
+            roles:u.roles,
+        }})
+        res.status(201)
+        next()
     })
 }
 
--- test/test11.users.js
+++ test/test11.users.js
@@ -16,13 +16,15 @@ async function login(username,password){
         uri:url("/login"),
         body,
         method:"POST",
-        json:true
+        json:true,
+        resolveWithFullResponse:true,
+        simple:false
     })
     return response
 }
 async function requestWithAuth(username,password,verb,uriPart,body){
     const loginResponse=await login(username,password)
-    const token=loginResponse.token
+    const token=loginResponse.body.token
     const response=await request({
         uri:url(uriPart),
         method:verb,
@@ -70,6 +72,70 @@ describe("Test /me",()=>{
             }
         }
         expect(r.body).to.be.jsonSchema(schema)
+    })
+
+    it("PUT /me",async ()=>{
+
+        // CHANGE THE USER
+        let user = { 
+            password: 'jiji',
+            email: 'jose.jose@gmail.com',
+            name: 'José Ignacio',
+            surname: 'Sbruzzi',
+        }
+
+        const rPut = await requestWithAuth("97452","jojo","PUT","/me",user)
+        console.log("================")
+        console.log("================")
+        console.log(rPut.body)
+        console.log("================")
+        console.log("================")
+
+
+        //TRY TO LOGIN AS BEFORE
+        const r0 = await login("97452","jojo")
+        expect(r0.statusCode).to.be.eq(401)
+
+
+        //TRY TO LOIGN WITH NEW CREDENTIALS
+        const r = await requestWithAuth("97452","jiji","GET","/me")
+        console.log("================")
+        console.log("================")
+        console.log(r.body)
+        console.log("================")
+        console.log("================")
+        let user2 = { 
+            username: '97452',
+            password: 'jiji',
+            email: 'jose.jose@gmail.com',
+            name: 'José Ignacio',
+            surname: 'Sbruzzi',
+            roles: [ 'students' ] 
+        }
+        let schema2={
+            required:["me"],
+            properties:{
+                "me":{const:user2}
+            }
+        }
+        expect(r.body).to.be.jsonSchema(schema2)
+
+
+        //BACK TO OLD CREDENTIALS
+        let user3 = { 
+            password: 'jojo',
+            email: 'jose.jose@gmail.com',
+            name: 'José Ignacio',
+            surname: 'Sbruzzi',
+        }
+
+        const rPut2 = await requestWithAuth("97452","jojo","PUT","/me",user3)
+        console.log("================")
+        console.log("================")
+        console.log(r.body)
+        console.log("================")
+        console.log("================")
+
     })
 
 })
