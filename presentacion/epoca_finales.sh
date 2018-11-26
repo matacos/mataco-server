333eb9ae13e4780241ab140509c737e68951013b
jisbruzzi <jose.sbru@gmail.com>
2018-11-26 18:44:20 -0300

    Fixes

--- presentacion/epoca_finales.sh
+++ presentacion/epoca_finales.sh
@@ -1,1 +1,3 @@
 http POST 'mataco2.herokuapp.com/api/set_now?now=2018-05-09'
+timedatectl set-ntp 0
+date +%Y%m%d -s "20180509"
--- presentacion/tiempo_inscripcion.sh
+++ presentacion/tiempo_inscripcion.sh
@@ -1,1 +1,3 @@
 http POST 'mataco2.herokuapp.com/api/set_now?now=2018-02-02'
+timedatectl set-ntp 0
+sudo date +%Y%m%d -s "20180202"
